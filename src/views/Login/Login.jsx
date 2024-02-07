dd13ef6385a44a9cd0cb5ce81ba16654dc33e64a
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-07 18:08:47 -0300

    añadir login sin iconos

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -4,9 +4,9 @@ export default function Login() {
   return (
     <div>
         <div className="d-flex justify-content-center align-items-center vh-100">  {/*<!----------------------------LOGIN--> */}
-            <div className="bg-white p-5 rounded-5 text-secondary" style="width: 25rem;">
+            <div className="bg-white p-5 rounded-5 text-secondary" style={{width: '25rem'}}>
                 <div className="d-flex justify-content-center">
-                    <img src="../images/iconos/logo_usuario.png" alt="icono_login" style="height: 7rem;" />
+                    <img src="../images/iconos/logo_usuario.png" alt="icono_login" style={{height: '7rem'}} />
                 </div>
                 <div>
                     <div className="text-center fs-1 fw-bold fst-italic">Inicio de sesión</div>
@@ -15,36 +15,36 @@ export default function Login() {
                     <div className="input-group">
                         <div className="input-group-text bg-$orange-500">
                             <img src="../images/iconos/logo_usuario2.png" alt="icono_nombredeusuario"
-                                style="height: 1rem;" />
+                                style={{height: '1rem'}} />
                         </div>
-                        <input className="form-control" type="text" placeholder="Nombre de usuario" />
+                        <input className="form-control" type="text" placeholder="Nombre de usuario"/>
                     </div>
                     <div>
                         <div className="input-group mt-1">
-                            <div className="input-group-text" style="background-color:#FF571E"/>
+                            <div className="input-group-text" style={{backgroundColor:'#FF571E'}}/>
                                 <img src="../images/iconos/logo_contraseña.png" alt="icono_contraseña"
-                                    style="height: 1rem;" />
+                                    style={{height: "1rem"}} />
                             </div>
                             <input className="form-control" type="password" placeholder="Contraseña" />
                         </div>
                     </div>
                     <div>
-                        <a href="#" className="fw-semibold fst-italic" style="font-size: 0.9rem">¿Ha olvidado su contraseña?</a>
+                        <a href="#" className="fw-semibold fst-italic" style={{fontSize: '0.9rem'}}>¿Ha olvidado su contraseña?</a>
                     </div>
                     <div className="btn btn-info text-white w-100 mt-4">Login</div>
                     <div className="btn btn-info text-white w-100 mt-1">Solo quiero mirar</div>
                     <div className="d-flex gap-1 justify-content-center mt-1">
                         <div>¿No tienes una cuenta?</div>
-                        <a href="#" className="fw-semibold fst-italic" style="font-size: 0.9rem">Regístrese</a>
+                        <a href="#" className="fw-semibold fst-italic" style={{fontsize: '0.9rem'}}>Regístrese</a>
                     </div>
                     <div className="py-3">
-                        <div className="border-bottom text-center" style="height:0.9rem">
+                        <div className="border-bottom text-center" style={{height:'0.9rem'}}>
                             <span className="bg-white px-3">o</span>
                         </div>
                     </div>
     
                     <div className="btn d-flex gap-2 justify-content-center border">
-                        <img src="../images/iconos/logo_google.png" alt="icono_google" style="height: 1.6rem;" />
+                        <img src="../images/iconos/logo_google.png" alt="icono_google" style={{height: '1.6rem'}} />
                         <div>Continuar con Google</div>
                     </div>
                 </div>
