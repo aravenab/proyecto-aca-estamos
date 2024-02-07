7470fe01906475ab3b2a042f83ab9dd531e49d18
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-07 19:14:52 -0300

    modificar navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div className="container-fluid-fluid">
       <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
         <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href=""></use></svg>
         </a>
 
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
