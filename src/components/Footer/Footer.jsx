51650be38a7ff51d4dbe1d4ef34f998f348a1114
Tomás Aravena <taravenab@uft.edu>
2024-02-07 15:14:58 -0300

    pruebas

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <div>
-      <h1>hola mundo hola mundo</h1>
+      <h1>hola mundo hola mundofooter</h1>
     </div>
   )
 }
