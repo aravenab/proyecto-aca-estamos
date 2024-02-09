0ae1102045b7857c483372149b064c63f075cb16
nariama <155675556+nariama@users.noreply.github.com>
2024-02-09 10:19:15 -0300

    cambios navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,10 +18,8 @@ export default function Navbar() {
 
         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
           <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" className="nav-link px-2 text-white">About</a></li>
+          <li><a href="#" className="nav-link px-2 text-white">Preguntas Frecuentes</a></li>
+          <li><a href="#" className="nav-link px-2 text-white">¿Quiénes somos?</a></li>
         </ul>
 
         <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
@@ -29,8 +27,8 @@ export default function Navbar() {
         </form>
 
         <div className="text-end">
-          <button type="button" className="btn btn-outline-light me-2">Login</button>
-          <button type="button" className="btn btn-warning">Sign-up</button>
+          <button type="button" className="btn btn-outline-light me-2">Inicia sesión</button>
+          <button type="button" className="btn btn-warning">Regístrate</button>
         </div>
       </div>
     </div>
