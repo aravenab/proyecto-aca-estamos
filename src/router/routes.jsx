0db1f487510dfe1dd2b505e3cecbec11f9bf348c
Tomás Aravena <taravenab@uft.edu>
2024-02-09 16:09:03 -0300

    no pude amigos, lo siento

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,21 +1,21 @@
-import { createBrowserRouter } from "react-router-dom";
-import Contacto from "../views/Contacto/Contacto.jsx";
-import Faq from "../views/Faq/Faq.jsx";
-import Login from "../views/Login/Login.jsx";
-import PerfilUsuario from "../views/PerfilUsuario/PerfilUsuario.jsx";
-import RegistroUsuariosEmpresas from "../views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas.jsx";
-import RegistroUusariosTalentos from "../views/RegistroUusariosTalentos/RegistroUusariosTalentos.jsx";
+// import { createBrowserRouter } from "react-router-dom";
+// import Contacto from "../views/Contacto/Contacto.jsx";
+// import Faq from "../views/Faq/Faq.jsx";
+// import Login from "../views/Login/Login.jsx";
+// import PerfilUsuario from "../views/PerfilUsuario/PerfilUsuario.jsx";
+// import RegistroUsuariosEmpresas from "../views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas.jsx";
+// import RegistroUusariosTalentos from "../views/RegistroUusariosTalentos/RegistroUusariosTalentos.jsx";
 
-const router = createBrowserRouter([
-    {path:"/", element: <Contacto/>},
-    {path:"/contacto", element:<Contacto/>},
-    {path: "faq", element: <Faq />},
-    {path: "login", element: <Login />},
-    {path: "perfil_usuario", element: < PerfilUsuario/>},
-    {path: "registro_usuarios_empresas", element: < RegistroUsuariosEmpresas/>},
-    {path: "registro_usuarios_talentos", element: < RegistroUusariosTalentos/>},
+// const router = createBrowserRouter([
+//     {path:"/", element: <Contacto/>},
+//     {path:"/contacto", element:<Contacto/>},
+//     {path: "faq", element: <Faq />},
+//     {path: "login", element: <Login />},
+//     {path: "perfil_usuario", element: < PerfilUsuario/>},
+//     {path: "registro_usuarios_empresas", element: < RegistroUsuariosEmpresas/>},
+//     {path: "registro_usuarios_talentos", element: < RegistroUusariosTalentos/>},
      
 
-]);
+// ]);
 
-export default router;
+// export default router;
