7bcca4b8da66a67bbbdfc7e5ceffc347afcea481
Tomás Aravena <taravenab@uft.edu>
2024-02-09 12:28:18 -0300

    cambios

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,6 +25,7 @@ export default function Footer() {
       <div className="col-6 col-md-2 mb-3">
         <h5>Section</h5>
         <ul className="nav flex-column">
+        <img src="../../assets/logo/f.jpg" />
           <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Home</a></li>
           <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Features</a></li>
           <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary">Pricing</a></li>
