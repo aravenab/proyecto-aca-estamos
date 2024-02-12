dc38af102bcdffff3cdad6aec962692f6b43e417
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-12 12:27:18 -0300

    arreglar cositas de index css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 //import { useState } from 'react'
 
 import './App.css'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import Faq from './views/Faq/Faq'
-import Contacto from './views/Contacto/Contacto'
-import Login from './views/Login/Login'
-import RegistroUsuariosEmpresas from './views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas'
-import RegistroUusariosTalentos from './views/RegistroUusariosTalentos/RegistroUusariosTalentos'
-import RegistrationForm from './views/Registro/Registro'
-import PerfilUsuario from './views/PerfilUsuario/PerfilUsuario'
-import ContactarTalento from './components/ContactarTalento/ContactarTalento'
-import CompletarPerfil from './components/CompletarPerfil/CompletarPerfil'
+// import Navbar from './components/Navbar/Navbar'
+// import Footer from './components/Footer/Footer'
+// import Faq from './views/Faq/Faq'
+// import Contacto from './views/Contacto/Contacto'
+// import Login from './views/Login/Login'
+// import RegistroUsuariosEmpresas from './views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas'
+// import RegistroUusariosTalentos from './views/RegistroUusariosTalentos/RegistroUusariosTalentos'
+// import RegistrationForm from './views/Registro/Registro'
+// import PerfilUsuario from './views/PerfilUsuario/PerfilUsuario'
+// import ContactarTalento from './components/ContactarTalento/ContactarTalento'
+// import CompletarPerfil from './components/CompletarPerfil/CompletarPerfil'
+import Inicio from './views/Inicio/Inicio'
 
 function App() {
   
@@ -21,8 +22,7 @@ function App() {
     <div>
 
       {/* Inserte el componente que desee visualizar*/}
-      <Navbar/>
-      <Faq/>
+      <Inicio/>
 
     </div>
   )
