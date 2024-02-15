ba2fdf7c36bac78c29b489901528b1f1594477ba
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-15 20:53:46 -0300

    push para seguir avanzando

---
+++ src/components/UneteANosotros/UneteANosotros.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+export default function 
+    <section id="services" class="services section-bg">
+      <div class="container">
+    
+        <div class="section-title" data-aos="fade-up">
+          <h2>¡Únete a nosotros!</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in
+            iste officiis commodi quidem hic quas.</p>
+        </div>
+    
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in">
+            <div class="icon-box icon-box-pink">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">1. Regístrate como Talento o Empresa</a></h4>
+              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
+                occaecati cupiditate</p>
+            </div>
+          </div>
+    
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box icon-box-cyan">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4 class="title"><a href="">2. Presentate ante el mundo</a></h4>
+              <p class="description">Podrás ver otros perfiles para inspirarte y destacar.</p>
+            </div>
+          </div>
+    
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box icon-box-green">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4 class="title"><a href="">3. Espera el llamado</a></h4>
+              <p class="description">Las empresas te encontrarán y contactarán.</p>
+            </div>
+          </div>
+    
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box icon-box-blue">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4 class="title"><a href="">4. ¡Disfruta!</a></h4>
+              <p class="description">Trabajando en tu propia comuna</p>
+            </div>
+          </div>
+    
+        </div>
+    
+      </div>
+    </section>() {
+  return (
+    <div>
+            <section id="services" class="services section-bg">
+      <div class="container">
+    
+        <div class="section-title" data-aos="fade-up">
+          <h2>¡Únete a nosotros!</h2>
+          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+            consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in
+            iste officiis commodi quidem hic quas.</p>
+        </div>
+    
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in">
+            <div class="icon-box icon-box-pink">
+              <div class="icon"><i class="bx bxl-dribbble"></i></div>
+              <h4 class="title"><a href="">1. Regístrate como Talento o Empresa</a></h4>
+              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint
+                occaecati cupiditate</p>
+            </div>
+          </div>
+    
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="100">
+            <div class="icon-box icon-box-cyan">
+              <div class="icon"><i class="bx bx-file"></i></div>
+              <h4 class="title"><a href="">2. Presentate ante el mundo</a></h4>
+              <p class="description">Podrás ver otros perfiles para inspirarte y destacar.</p>
+            </div>
+          </div>
+    
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="200">
+            <div class="icon-box icon-box-green">
+              <div class="icon"><i class="bx bx-tachometer"></i></div>
+              <h4 class="title"><a href="">3. Espera el llamado</a></h4>
+              <p class="description">Las empresas te encontrarán y contactarán.</p>
+            </div>
+          </div>
+    
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0" data-aos="zoom-in" data-aos-delay="300">
+            <div class="icon-box icon-box-blue">
+              <div class="icon"><i class="bx bx-world"></i></div>
+              <h4 class="title"><a href="">4. ¡Disfruta!</a></h4>
+              <p class="description">Trabajando en tu propia comuna</p>
+            </div>
+          </div>
+    
+        </div>
+    
+      </div>
+    </section>
+    </div>
+  )
+}
