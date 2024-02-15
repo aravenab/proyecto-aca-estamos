ae1a61d2addbf0cd0d945bd324fd349d95c03256
emmanuel gonzalez <emmanuel.gonzalez903@gmail.com>
2024-02-15 17:15:45 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 //import { useState } from 'react'
 
-import './App.css'
+
 import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import Faq from './views/Faq/Faq'
-import Contacto from './views/Contacto/Contacto'
-import Login from './views/Login/Login'
-import RegistroUsuariosEmpresas from './views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas'
-import RegistroUusariosTalentos from './views/RegistroUusariosTalentos/RegistroUusariosTalentos'
-import RegistrationForm from './views/Registro/Registro'
-import PerfilUsuario from './views/PerfilUsuario/PerfilUsuario'
-import ContactarTalento from './components/ContactarTalento/ContactarTalento'
-import CompletarPerfil from './components/CompletarPerfil/CompletarPerfil'
-import Inicio from './views/Inicio/Inicio'
-import Index from './views/Index/Index'
 
 function App() {
   
@@ -23,8 +11,8 @@ function App() {
     <div>
 
       {/* Inserte el componente que desee visualizar*/}
-    
-      <Index/>
+      <Navbar />
+      {/* <Index/> */}
     </div>
   )
 }
