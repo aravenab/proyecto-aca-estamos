1b8c03c5a2f29217b4d297ee9a2339531861f7bc
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-15 18:12:00 -0300

    Faq visualizar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@
 import './App.css'
 // import Navbar from './components/Navbar/Navbar'
 // import Footer from './components/Footer/Footer'
-// import Faq from './views/Faq/Faq'
+import Faq from './views/Faq/Faq'
 // import Contacto from './views/Contacto/Contacto'
 // import Login from './views/Login/Login'
 // import RegistroUsuariosEmpresas from './views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas'
@@ -22,7 +22,7 @@ function App() {
     <div>
 
       {/* Inserte el componente que desee visualizar*/}
-      <Inicio/>
+      <Faq/>
 
     </div>
   )
