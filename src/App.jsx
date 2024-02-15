74f61d98ca663417d883d1e3d43c06fa039e879b
Tomás Aravena <taravenab@uft.edu>
2024-02-15 12:38:24 -0300

    commit todo proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 //import { useState } from 'react'
 
 import './App.css'
-// import Navbar from './components/Navbar/Navbar'
-// import Footer from './components/Footer/Footer'
-// import Faq from './views/Faq/Faq'
-// import Contacto from './views/Contacto/Contacto'
-// import Login from './views/Login/Login'
-// import RegistroUsuariosEmpresas from './views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas'
-// import RegistroUusariosTalentos from './views/RegistroUusariosTalentos/RegistroUusariosTalentos'
-// import RegistrationForm from './views/Registro/Registro'
-// import PerfilUsuario from './views/PerfilUsuario/PerfilUsuario'
-// import ContactarTalento from './components/ContactarTalento/ContactarTalento'
-// import CompletarPerfil from './components/CompletarPerfil/CompletarPerfil'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
+import Faq from './views/Faq/Faq'
+import Contacto from './views/Contacto/Contacto'
+import Login from './views/Login/Login'
+import RegistroUsuariosEmpresas from './views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas'
+import RegistroUusariosTalentos from './views/RegistroUusariosTalentos/RegistroUusariosTalentos'
+import RegistrationForm from './views/Registro/Registro'
+import PerfilUsuario from './views/PerfilUsuario/PerfilUsuario'
+import ContactarTalento from './components/ContactarTalento/ContactarTalento'
+import CompletarPerfil from './components/CompletarPerfil/CompletarPerfil'
 import Inicio from './views/Inicio/Inicio'
 
 function App() {
@@ -22,7 +22,9 @@ function App() {
     <div>
 
       {/* Inserte el componente que desee visualizar*/}
-      <Inicio/>
+      <Navbar/>
+      <Faq/>
+      <Footer/>
 
     </div>
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,56 +1,119 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from "react";
+import './navbar.css'
 
 export default function Navbar() {
-
   useEffect(() => {
     // Inicializar el popover cuando el componente se monte
-    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
-    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
-    
+    const popoverTriggerList = document.querySelectorAll(
+      '[data-bs-toggle="popover"]'
+    );
+    const popoverList = [...popoverTriggerList].map(
+      (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+    );
+
     // Limpia el popover cuando el componente se desmonte
     return () => {
-        popoverList.forEach(popover => popover.dispose());
+      popoverList.forEach((popover) => popover.dispose());
     };
-}, []); // [] como segundo argumento para que solo se ejecute una vez al montar el componente
-
+  }, []); // [] como segundo argumento para que solo se ejecute una vez al montar el componente
 
   return (
-    <header className="container-fluid-fluid p-3 text-bg-dark">
-    <div className="container-fluid-fluid">
-    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-        <svg className="bi me-2" width="40" height="32"><use xlink:href=".../assets/logo"></use></svg>
-        <span className="fs-4">Simple header</span>
-      </a>
-      <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
-            {/* <use xlink:href="/../assets/logo/logo_navbar.svg"></use> */}
-            <image xlinkHref="..\assets\logo\logo_navbar.png" width="40" height="32" />
-          </svg>
-        </a>
-
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li class="nav-item">
-              <button type="button" className="btn btn-lg" data-bs-toggle="popover"
-                data-bs-title="Notificaciones" data-bs-content="No hay notificaciones disponibles" data-bs-placement="bottom">
-                <i className="bi bi-bell" style={{color: "white"}}></i>
-              </button>
-            </li>
-            <li><a href="#" className="nav-link px-2 text-white">Contacto</a></li>
-            <li><a href="#" className="nav-link px-2 text-white">Preguntas Frecuentes</a></li>
-            <li><a href="#" className="nav-link px-2 text-white">¿Quiénes somos?</a></li>
-          </ul>
-
-        <form className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" className="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search"/>
-        </form>
-
-        <div className="text-end">
-          <button type="button" className="btn btn-outline-light me-2">Inicia sesión</button>
-          <button type="button" className="btn btn-warning">Regístrate</button>
+    // <!-- ======= Header ======= -->
+    <header id="header" className="fixed-top d-flex align-items-center">
+      <div className="container d-flex align-items-center">
+        <div className="logo me-auto">
+          <h1>
+            <a href="index.html">Acá estamos</a>
+          </h1>
+          {/* <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" className="img-fluid"></a>--> */}
         </div>
+
+        <nav id="navbar" className="navbar order-last order-lg-0" />
+        <ul>
+          {/* <!-- Ícono de notificación a la derecha --> */}
+          <li className="nav-link scrollto">
+            <button
+              type="button"
+              className="btn btn-lg"
+              data-bs-toggle="popover"
+              data-bs-title="Notificaciones"
+              data-bs-content="No hay notificaciones disponibles"
+            >
+              <i className="bi bi-bell"></i>
+            </button>
+          </li>
+          <li>
+            <a className="nav-link scrollto active" href="#hero">
+              Inicio{" "}
+            </a>
+          </li>
+          <li className="dropdown">
+            <a href="#">
+              <span>Acerca de</span> <i className="bi bi-chevron-down"></i>
+            </a>
+            <ul>
+              <li>
+                <a className="nav-link scrollto" href="#about">
+                  ¿Quienes somos?
+                </a>
+              </li>
+              <li>
+                <a className="nav-link scrollto" href="#testimonials">
+                  Testimonios
+                </a>
+              </li>
+              <li>
+                <a className="nav-link scrollto" href="#team">
+                  Nuestros Talentos
+                </a>
+              </li>
+              <li>
+                <a className="nav-link scrollto" href="#clients">
+                  Marcas
+                </a>
+              </li>
+              <li className="dropdown">
+                <a href="#">
+                  <span>Deep Drop Down</span>{" "}
+                  <i className="bi bi-chevron-right"></i>
+                </a>
+                <ul>
+                  <li>
+                    <a href="#">Deep Drop Down 1</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Drop Down 2</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Drop Down 3</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Drop Down 4</a>
+                  </li>
+                  <li>
+                    <a href="#">Deep Drop Down 5</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a className="nav-link scrollto" href="#services">
+              ¡Únete!
+            </a>
+          </li>
+          {/* <!-- <li><a className="nav-link scrollto " href="#portfolio">Nuestros perfiles</a></li>
+                      <li><a className="nav-link scrollto" href="#clients">Nuestros clientes</a></li> --> */}
+          <li>
+            <a className="nav-link scrollto" href="#contact">
+              Contacto
+            </a>
+          </li>
+        </ul>
+        <i className="bi bi-list mobile-nav-toggle"></i>
+        {/* </nav><!-- .navbar --> */}
       </div>
-    </div>
-  </header>
-  )
+    </header>
+  );
 }
