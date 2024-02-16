d85b235135b45c9910f3d350a96388b980716c57
Osvaldo Sepulveda <osvaldo.sepulveda.v@ug.uchile.cl>
2024-02-15 22:17:36 -0300

    añadir cartas de empresas

---
+++ src/components/CartasEmpresas/CartasEmpresasItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CartasEmpresasItem = ({ imgSrc, title, category }) => (
+  <div className={`col-lg-4 col-md-6 portfolio-item filter-${category}`}>
+    <div className="portfolio-wrap">
+      <img src={imgSrc} className="img-fluid" alt="" />
+      <div className="portfolio-info">
+        <h4>{title}</h4>
+        <p>{category}</p>
+      </div>
+      <div className="portfolio-links">
+        <a href={imgSrc} data-gallery="portfolioGallery" className="portfolio-lightbox" title={title}><i className="bx bx-plus"></i></a>
+        <a href="portfolio-details.html" title="More Details"><i className="bx bx-link"></i></a>
+      </div>
+    </div>
+  </div>
+);
+
+export default CartasEmpresasItem;
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,12 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom' //Esto es para rutear
 import router from './router/routes.jsx' // importamos nuestro router
 import UneteANosotros from './components/UneteANosotros/UneteANosotros.jsx'
+import CartasEmpresas from './components/CartasEmpresas/CartasEmpresas.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <RouterProvider router ={router}/> 
-   {/* <App />   */}
-   {/* <UneteANosotros/> */}
+      {/* <RouterProvider router ={router}/>  */}
+      <CartasEmpresas/>
   </React.StrictMode>,
 )
