d1b9a0b1eba7d44c0ba3ef118ee29b4d2442a520
Tomás Aravena <taravenab@uft.edu>
2024-02-15 21:29:09 -0300

    contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -32,8 +32,8 @@ const Contact = () => {
                 <h4>Teléfono:</h4>
                 <p>+1 5589 55488 55s</p>
               </div>
+              <div style={{ width: '100%' }}><iframe title="Mapa" width="100%" height="300" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=es&amp;q=Blanco%20Encalada%201335+(Municipalidad%20de%20Renca)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" allowFullScreen></iframe></div>
 
-              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>
             </div>
 
           </div>
