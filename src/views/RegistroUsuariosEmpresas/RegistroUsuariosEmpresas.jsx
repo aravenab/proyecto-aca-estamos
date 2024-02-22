c7d31fe5a212813ede5624386256da4b2d72a87c
Tomás Aravena <taravenab@uft.edu>
2024-02-21 21:21:38 -0300

    ingresando validación de formularios

--- src/views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas.jsx
+++ src/views/RegistroUsuariosEmpresas/RegistroUsuariosEmpresas.jsx
@@ -16,7 +16,7 @@ const formEnviado = () =>{
   return (
         <div>
             <div className="container-fluid m-5 bg-light shadow border-5 pt-3">
-                <form className="container position-relative">
+                <form className="container position-relative needs-validation" noValidate>
                     <h1>Registro como EMPRESA</h1>
                     <div className="row">
                         <div className="col-3">
@@ -34,22 +34,30 @@ const formEnviado = () =>{
                       </div>
                     <div className="row">
                         <div className="col-6 col-sm-6 mb-3">
-                            <label className="col-12 form-label" htmlFor="name">Nombre de empresa</label>
-                            <input className="form-control" id="name" type="text" placeholder="Nombre de empresa" />
+                            <label className="col-12 form-label" htmlFor="name">Nombre de la empresa</label>
+                            <input className="form-control" id="name" type="text" placeholder="Nombre de empresa" required/>
+                            <div className="invalid-feedback"> Ingresa el Nombre de la empresa</div>
+                            <div className='valid-feedback'> Nombre ingresado correctamente</div>
                         </div>
                         <div className="col-6 col-sm-6 mb-3">
                             <label className="form-label" htmlFor="email">Email Corporativo</label>
-                            <input className="form-control" id="email" type="email" placeholder="Email corporativo" />
+                            <input className="form-control" id="email" type="email" placeholder="Email corporativo" required/>
+                            <div className="invalid-feedback"> Porfavor, ingresa el Email corporativo</div>
+                            <div className='valid-feedback'> Email corporativo ingresado correctamente </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-6">
                             <label className="col-12 form-label" htmlFor="rut">Rut de la empresa</label>
-                            <input className="form-control" id="rut" type="number" placeholder="Rut de la empresa" />
+                            <input className="form-control" id="rut" type="number" placeholder="Rut de la empresa"required />
+                            <div className="invalid-feedback"> Ingresa el Rut de la empresa </div>
+                            <div className='valid-feedback'> Rut de la empresa correcto</div>
                         </div>
                         <div className="col-6 col-sm-6 mb-3">
                             <label className="col-12 form-label" htmlFor="giro">Giro o rubro</label>
-                            <input className="form-control" id="giro" type="text" placeholder="Giro o rubro" />
+                            <input className="form-control" id="giro" type="text" placeholder="Giro o rubro"required />
+                            <div className="invalid-feedback"> Porfavor, ingresa el Giro o rubro</div>
+                            <div className='valid-feedback'> Gracias por ingresar el Giro o rubro </div>
                         </div>
                     </div>
                     <div className="row">
@@ -92,6 +100,26 @@ const formEnviado = () =>{
                 </form>
             </div>
             <img src="" className="img-fluid mx-auto d-block mb-2" alt="" />
+            <script>
+  {(() => {
+    'use strict';
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add('was-validated');
+        }
+      }, false);
+    });
+  })()}
+</script>
+
         </div>
     )
 }
